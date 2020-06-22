c981c874f0956028ac1bac41f170c2c83f91a7e9
Taylor Olson <pipestay622@gmail.com>
2020-06-21 18:58:55 -0700

    at checkbox

--- ReadMe.md
+++ ReadMe.md
@@ -48,10 +48,10 @@ Follow these steps to set up and work on your project:
 Your MVP should include, at a minimum, the following components.
 
 - [x] A homepage that has a "/" route and links to your form (button, nav bar, or any other type of link is acceptable)
-- [ ] A form with a "/pizza" route
-- [ ] A name text input field
+- [x] A form with a "/pizza" route
+- [x] A name text input field
 - [ ] Validation for name - name must be at least 2 characters
-- [ ] Dropdown form component for pizza size
+- [x] Dropdown form component for pizza size
 - [ ] Checklist form component for toppings - at least 4 (hint: name each separately!)
 - [ ] Text input form component for special instructions
 - [ ] An Add to Order button that submits form and returns a database record of name, pizza size, sauce, and special instructions
--- src/Pizza.js
+++ src/Pizza.js
@@ -4,8 +4,47 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Pizza = () => {
     return (
-        <h1>Pizza Form</h1>
+        <div className = "Pizza">
+            <h1>BUILD YOUR OWN PIZZA</h1>
+            <hr></hr>
+
+
+            <h2>Name:</h2>
+                <p>Required *</p>
+                <form>
+                    <label>
+                        Name
+                        <input type="text" name="name" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            <hr></hr>
+
+
+            <h2>Choice of size:</h2>
+                <p>Required *</p>
+                <select>
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option selected value="large">Large</option>
+                </select>
+            <hr></hr>
+
+
+            <h2>Add Toppings:</h2>
+            <p>Choose up to 10!</p>
+            
+            <hr></hr>
+
+
+            <h2>Special Instructions:</h2>
+            <p>Anything else we can do for you?</p>
+            <hr></hr>
+
+
+            <button>Submit Order</button>
+        </div>
     );
-  };
+};
   
-  export default Pizza;
+export default Pizza;
